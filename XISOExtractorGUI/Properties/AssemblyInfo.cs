cc5e783eac1731a8039cb39353af74cd9090d79b
Swizzy <swizzy@hotmail.co.uk>
2014-03-03 02:28:39 +0100

    Minor corrections
    Signed-off-by: Swizzy <swizzy@hotmail.co.uk>

--- XISOExtractorGUI/Properties/AssemblyInfo.cs
+++ XISOExtractorGUI/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ExisoGUI")]
+[assembly: AssemblyTitle("XISOExtractorGUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ExisoGUI")]
+[assembly: AssemblyProduct("XISOExtractorGUI")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
